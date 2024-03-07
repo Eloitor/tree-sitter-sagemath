17664dd10113aa05721d2ad077553a27f6ab9372
Eloi Torrents <eloitor@disroot.org>
2024-03-07 15:44:47 +0100

    Rename tree-sitter-python to tree-sitter-sagemath

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -22,14 +22,14 @@
 use tree_sitter::Language;
 
 extern "C" {
-    fn tree_sitter_python() -> Language;
+    fn tree_sitter_sagemath() -> Language;
 }
 
 /// Get the tree-sitter [Language][] for this grammar.
 ///
 /// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
 pub fn language() -> Language {
-    unsafe { tree_sitter_python() }
+    unsafe { tree_sitter_sagemath() }
 }
 
 /// The content of the [`node-types.json`][] file for this grammar.
@@ -47,6 +47,6 @@ mod tests {
         let mut parser = tree_sitter::Parser::new();
         parser
             .set_language(&super::language())
-            .expect("Error loading Python grammar");
+            .expect("Error loading Sagemath grammar");
     }
 }
